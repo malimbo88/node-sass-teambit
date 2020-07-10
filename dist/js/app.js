fb8e3050a5c65898d98d7dfb94c89bcd6201299a
Nicola <nicolavalente88@gmail.com>
2020-07-10 16:38:20 +0200

    Prova 3

--- dist/js/app.js
+++ dist/js/app.js
@@ -93,7 +93,26 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+//javaScript
+//jquery
+//Header Menu Dropdown
+$(document).ready(function () {
+  $(".with_dropdown").hover(function () {
+    var menuDropdownHtml = $(this).children(".menu_dropdown");
+    var menuDropdownIcon = $(this).find(".menu_inline > i"); //Cambio icona menu Dropdown
 
+    if (menuDropdownIcon.hasClass("fa-chevron-down")) {
+      menuDropdownIcon.removeClass("fa-chevron-down");
+      menuDropdownIcon.addClass("fa-chevron-up");
+    } else if (menuDropdownIcon.hasClass("fa-chevron-up")) {
+      menuDropdownIcon.removeClass("fa-chevron-up");
+      menuDropdownIcon.addClass("fa-chevron-down");
+    } //Mostro o nascondo menu Dropdown attraverso class visible
+
+
+    menuDropdownHtml.toggleClass("visible");
+  });
+}); //end jquery
 
 /***/ }),
 
